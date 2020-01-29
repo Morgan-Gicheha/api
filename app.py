fd8f751824b977af4ff880a899aba6cfe8e7f350
codeGiche <morgangicheha4@gmail.com>
2020-01-29 16:05:42 +0300

    auth api

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash,check_password_hash
+import jwt
+import datetime
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:morgan@127.0.0.1:5432/api_test'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:morgan8514@127.0.0.1:5432/api_test'
 app.config['SECRET_KEY']='secretkey'
 
 db=SQLAlchemy(app)
@@ -28,7 +30,7 @@ class Todo(db.Model):
     id= db.Column(db.Integer,primary_key=True)
     text= db.Column(db.String(200))
     complete=db.Column(db.Boolean)
-    user_id= db.Column(db.Integer)
+    public_id= db.Column(db.Integer)
 
 User.create()
 
@@ -38,9 +40,9 @@ def create_user():
 
     data= request.get_json()
 
-    hashed_password = generate_password_hash(data['password_j'],method='sha256')
+    hashed_password = generate_password_hash(data['password'],method='sha256')
 
-    new_user= User(public_id=str(uuid.uuid4()), name=data['name_j'], password=hashed_password, admin=False)
+    new_user= User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
 
     db.session.add(new_user)
     db.session.commit()
@@ -65,17 +67,69 @@ def get_all_users():
 
     return jsonify({"user":output})
 
-@app.route('/user/<user_id>',methods=['GET'])
-def get_one_user():
-    return ''
+@app.route('/user/<public_id>',methods=['GET'])
+def get_one_user(public_id):
 
-@app.route('/user/<user_id>',methods=['PUT'])
-def promote_user():
-    return ''
+    single_user=User.query.filter_by(public_id=public_id).first()
+    if not single_user:
+        return jsonify({"message":"no user found!"})
 
-@app.route('/user/<user_id>',methods=['DELETE'])
-def delete_user():
-    return ''
+    user_data={}
+    user_data['public_id']= single_user.public_id
+    user_data['name']= single_user.name
+    user_data['password']=single_user.password
+    user_data['admin']=single_user.admin
+
+    return jsonify({"message": user_data})
+
+@app.route('/user/<public_id>',methods=['PUT'])
+def promote_user(public_id):
+
+    single_user=User.query.filter_by(public_id=public_id).first()
+    if not single_user:
+        return jsonify({"message":" no user found! "})
+    single_user.admin=True
+    db.session.commit()
+
+    return jsonify({"message" : "User has been promoted!"})
+
+@app.route('/user/<public_id>',methods=['DELETE'])
+def delete_user(public_id):
+        
+    single_user=User.query.filter_by(public_id=public_id).first()
+    if not single_user:
+        return jsonify({"message":" no user found! "})
+
+    db.session.delete(single_user)
+    db.session.commit()
+
+    return jsonify({"message":"user deleted!"})
+
+# login route
+@app.route('/login')
+def login():
+
+    auth=request.authorization 
+    if not auth or not auth.username or not auth.password:
+        return make_response ('could not verify!',401, {'WWW-Authenticate':'Basic realm="Login required!'})
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response ('could not verify!',401, {'WWW-Authenticate':'Basic realm="Login required!'})
+
+    # to compare hasshed password yo use check_password_harsh
+    # comparing text password to inputed password. if it is correct, we now generate the token
+
+    if check_password_hash(user.password,auth.password):
+        # creating token
+        token = jwt.encode({"public_id": user.public_id, "exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'] )
+        # returning the token as a jason object
+        return jsonify({"token" : token.decode('UTF-8')})
+
+    # if the password is wrong it returns the error message below
+    return make_response ('could not verify!',401, {'WWW-Authenticate':'Basic realm="Login required!'})
+
 
 
 
